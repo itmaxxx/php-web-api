27fb69557195d98072476b04cef671b4873114b6
itmaxxx <dmitrievmaks77@gmail.com>
2021-12-22 11:00:46 +0200

    Add db seed, refactor db drop

--- db/db.controller.php
+++ db/db.controller.php
@@ -3,17 +3,38 @@
 class DbController {
   private $conn;
   
-  public function __constructor($conn) {
+  public function __construct($conn) {
     $this->conn = $conn;
 
+    $this->drop(['Users']);
+
     $this->initialize();
+
+    $this->seed('Users', ["id", "name", "age"], [
+      [1, "Max Dmitriev", 21]
+    ]);
   }
 
-  private function drop() {
+  public function seed(string $table, array $fields, array $fixtures) {
     try {
-      # Drop Users
-      $sql = "DROP TABLE IF EXISTS Users";
-      $conn->exec($sql);
+      $fieldsString = implode(",", $fields);
+      $valuesString = implode(",", array_fill(0, count($fields), "?"));
+
+      foreach ($fixtures as $fixture) {
+        $sql = "INSERT INTO $table ($fieldsString) VALUES ($valuesString)";
+        $this->conn->prepare($sql)->execute($fixture);
+      }
+    } catch (Exception $ex) {
+      httpException("Failed to seed db, table '$table'", 500)['end']();
+    }
+  }
+
+  private function drop($tables) {
+    try {
+      foreach ($tables as $table) {
+        $sql = "DROP TABLE IF EXISTS $table";
+        $this->conn->exec($sql);
+      }
     } catch (Exception $ex) {
       httpException("Failed to drop db", 500);
     }
